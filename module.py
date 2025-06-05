7f158c071b6f3f6974cbcc128ef2e5956c5a1700
Sameer jaga <lalit.03@icloud.com>
2025-06-05 09:18:12 +0000

    we are learn about module and pip of python and how its work

---
+++ module.py
@@ -0,0 +1,23 @@
+#MODULE = A moaudlue  is a file conataing code written by some body and we can use after import in our system.
+#manyt type of module These are modules developed by others and not included in Python by default. You can install them using pip.
+
+# Examples:
+
+# numpy – for numerical computations
+
+# pandas – for data manipulation
+
+# requests – for HTTP requests
+
+# flask – for web development
+
+# opencv-python – for image and video processing
+# they are extrnal module  . how we can importan in our device ? using help of pip 
+# pip is the paclkage manger for python 
+# example write in treminal : pip insatll numpy
+# after module will dawload in your device 
+# i am using pyjokes mnaodule  frisl;ty i dawlond the module in my system  using pip
+import pyjokes;
+jokes = pyjokes.get_joke()
+print(jokes);
+#  example   write program to print the contents of directry using the  os  module  serach online for the function which does that 
