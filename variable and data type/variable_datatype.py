96fde9011e897c7c15ac8c4fa7463e52b72eba04
Sameer jaga <lalit.03@icloud.com>
2025-06-06 02:45:38 +0000

    variable and datad type

---
+++ variable and data type/variable_datatype.py
@@ -0,0 +1,52 @@
+# A variable name to given a loction in program variable is just name of data house variable is a identy .
+# srting always wriet in  double quets  eg : b = "sameer"
+
+# Data type in python uselly having four data type
+#  1. integers =1,2,3...
+# 2. strings = "_____"
+# 3. boolen = true/ flase
+# 5 none = undefind 
+# floating = 0.22,0.28,0.58.....
+
+# opreters in python 
+# 1.arthmetic opreter = +,-,*,/
+# 2.assigment opreter= =, +=, -=
+# 3.comparision opreter= ==,>,>=,<=
+# 4.logical opreter =&,| |,!=
+#  a= 7 + b= 9 =
+# a and b are opends + is opreter
+ 
+
+#  type() function is used to find the data type of given variable in python
+# example .
+# a =7 ;
+# t =type(a);
+# print(t);
+#  output is int (becoz a is int type )
+#  we can change the data type of variable  data type 
+# example : 
+# a ="7.5"(this is actul;ly string )
+# b = float(a);
+# t = type(b);
+# print(t);
+
+
+
+
+
+
+
+# INPUT FUNCTION 
+# This function allows to the user to take input from the key board  as string 
+# using of santax 
+# variable = input("enter the variable value");
+#  a = datatype(input("enter the valuer"))
+# write a program  add two numbers inoput by user 
+a = int(input("enter the value of a"));
+b= int(input("enter the value of b")) ;
+print(a+b)
+# Q2 check the number who is greater than and tello what type of data
+e=8;
+f=9;
+c =e>f;
+print(type(c))
