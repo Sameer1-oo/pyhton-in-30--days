3a362798a86710780f609f4d3d5927c0a805c75d
Sameer jaga <lalit.03@icloud.com>
2025-06-06 03:15:50 +0000

    some basic string practice sets

---
+++ string function/02_problem.py
@@ -0,0 +1,9 @@
+# write a program to fill in letter tampalte  given below with name and  that name and date 
+# ''' leeter = dear <|name|>
+#  your are sellceted
+#  <||date|>'''
+
+letter ='''dear <|name|>
+ your are sellceted
+ <||date|>'''
+print(letter.replace("<|name|>" , "sameer").replace("<|date|>" , "2/22/2050"))
---
+++ string function/03_problem.py
@@ -0,0 +1,3 @@
+# write the program dectected double sapce 
+sameer = "sameer is a brave  boy i have "
+print(sameer.find("  "))
