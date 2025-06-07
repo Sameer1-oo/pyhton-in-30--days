1009a50379e65f05ac21ce689eebcfe4d44bb9c9
Sameer jaga <lalit.03@icloud.com>
2025-06-07 05:02:14 +0000

    practice problem 2

---
+++ list and tuples/02_problem.py
@@ -0,0 +1,19 @@
+# write a proghere :  to accept marks in of 6 student abc   in shot list numbert 
+marks = []
+
+f1 =  int(input("enter the marks here :  "))
+marks.append(f1)
+f2 =  int(input("enter the marks here :  "))
+marks.append(f2)
+f3 =  int(input("enter the marks here :  "))
+marks.append(f3)
+f4 =  int(input("enter the marks here :  "))
+marks.append(f4)
+f5 =  int(input("enter the marks here :  "))
+marks.append(f5)
+f6 =  int(input("enter the marks here :  "))
+marks.append(f6)
+f7 =  int(input("enter the marks here :  "))
+marks.append(f7)
+marks.sort()
+print(marks)
