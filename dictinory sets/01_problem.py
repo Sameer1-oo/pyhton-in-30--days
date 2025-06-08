2641a121d697d9338095fdb2a4ac96fe7d5bd91e
Sameer jaga <lalit.03@icloud.com>
2025-06-08 03:45:57 +0000

    practice set probelm

---
+++ dictinory sets/01_problem.py
@@ -0,0 +1,10 @@
+# write a progrmme  to create a dictionary of hindi words and value a nd there english UnicodeTranslateError
+# provided user  with an option  to took it up
+words = {"madad" : "help",
+         "kursi" : "chair",
+         "billi" :   " cat"
+}
+word = input("enter the word  :  ")
+print  (words[word])
+
+        
