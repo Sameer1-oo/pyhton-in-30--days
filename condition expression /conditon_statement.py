3fb038e6f142329bb93cc68ebaabb28b2080cb1b
Sameer jaga <lalit.03@icloud.com>
2025-06-09 03:49:13 +0000

    condition satement

---
+++ condition expression /conditon_statement.py
@@ -0,0 +1,58 @@
+# condition if condition saticefied then the code will be exicute 
+#  examnple 
+a=2
+if(a==3):
+    print("a is three")
+else:
+    print("lesser than three"
+          )
+    
+    # example2
+    age = int(input("enter the age "))
+    if (age>=18 ):
+        print("your the adult and you can go in pub bar")
+    elif(age==20):
+        print("you are in  20 s age")
+    else :
+        print("yopu are not able and adult")
+
+print("end of program")
+ 
+
+
+#  n Python, elif stands for "else if". It is used in conditional statements to check multiple conditions.
+
+# 🔹 Syntax:
+# t
+# if condition1:
+#     # code block if condition1 is true
+# elif condition2:
+#     # code block if condition2 is true
+# elif condition3:
+#     # code block if condition3 is true
+# else:
+#     # code block if none of the above conditions are true
+# 🔸 Example:
+# t
+num = 15
+
+if num < 10:
+    print("Less than 10")
+elif num == 10:
+    print("Equal to 10")
+elif num < 20:
+    print("Less than 20")
+else:
+    print("20 or more")
+# Output:
+
+# \
+# Less than 20
+# ✅ Key Points:
+# You can use multiple elif blocks.
+
+# else is optional but should come at the end.
+
+# Python checks conditions from top to bottom and stops when one is true.
+
+# Let me know if you want a fun real-life example too!
