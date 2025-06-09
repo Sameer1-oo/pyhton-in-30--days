f1d08b17beceb67abe00904539c465861219a61f
Sameer jaga <lalit.03@icloud.com>
2025-06-09 04:30:55 +0000

    practcie set problem 1

---
+++ condition expression /01_problem.py
@@ -0,0 +1,14 @@
+# write a program find the gresets1 four number input by user 
+a1 = int(input("enter the number a1:"))
+a2= int(input("enter the number a2 :"))
+a3 = int(input("enter the number a3 :"))
+a4 = int(input("enter the number a4 :"))
+if (a1>a2 and a1>3 and a1>a4):
+ print("a1 is the grestes :" ,a1)
+elif(a2>a3 and a2>a1 and a2>a4):
+  print("a2 is the grestest : ", a2)
+elif(a3>a2 and a3>a1 and a3>a4):
+  print("a3 is the grestest value : ", a3)
+elif( a4>a1 and a4>a2 and a4>a3):
+  print("a4 is the grestest number :",a4)
+
