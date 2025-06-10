c0c37e70160ff5a03abf2894018723c4eb25f577
Sameer jaga <lalit.03@icloud.com>
2025-06-10 16:47:10 +0000

    loop concept

---
+++ loops concept /loops.py
@@ -0,0 +1,31 @@
+#  loops 
+# some time we want to repat the statement in oour program intanse print  1 to 1000 
+# loop make easy  for a program 
+# they are two type of loops
+# 1. while Loop
+# 2.for loops
+
+
+
+
+# while lopps
+
+# while(condition ):
+#  in the while loop when your condtion is checked if it evealuted and to true body  is executed 
+
+#  example
+i=0
+while (i<10):
+    print("hello")
+    i+=1
+
+     # simple concept is that loops oinly to make easy work when we do more repted work i can possible in 3-2 line 
+     #  
+    #  for loops
+
+    #  a for lop is used to itrate through a sequnce like list truple and string /
+l=[11,"asamee,", 1.3,4]
+for i in l:
+    print(i)
+    
+    
