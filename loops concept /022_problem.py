2041dc0efadd7a0c7adbecc128dbfde24199e6f9
Sameer jaga <lalit.03@icloud.com>
2025-06-11 04:36:22 +0000

    for loop concept

---
+++ loops concept /022_problem.py
@@ -0,0 +1,5 @@
+# wriet a program to greet user that name start with s 
+l = ['sameer', 'sakshi','rahul','renu']
+for name in l :
+    if(name.startswith('s')):
+        print("hello lovely couple" ,name)
